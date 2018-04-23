24dbd69868fcef7e460337809f1b744bc61397df
Jaime Lopez <detec.azul@gmail.com>
2018-04-23 17:01:30 +0100

    MVP done. User can input words and the app returns the number of words

--- WordCounter/app/src/main/java/com/example/detectiveazul/wordcounter/WordCounterActivity.java
+++ WordCounter/app/src/main/java/com/example/detectiveazul/wordcounter/WordCounterActivity.java
@@ -2,12 +2,35 @@ package com.example.detectiveazul.wordcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class WordCounterActivity extends AppCompatActivity {
+    private EditText wordsToCountText;
+    private Button countButton;
+    private TextView countView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_word_counter);
+        wordsToCountText = findViewById(R.id.wordsToCountText);
+        countButton = findViewById(R.id.countButton);
+        countView = findViewById(R.id.countView);
+    }
+
+    public String getWordsToCountText() {
+        return wordsToCountText.getText().toString();
+    }
+
+    public void setCountView(String wordCount) {
+        this.countView.setText(wordCount);
+    }
+
+    public void onCountButtonClicked(View view) {
+        String string = "" + WordCounter.wordCount(getWordsToCountText());
+        setCountView(string);
     }
 }
